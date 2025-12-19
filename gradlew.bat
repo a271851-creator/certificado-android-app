6730be0b6999d55b7b9e6a9e0c785a3b5998e423
Saty <a271851@uach.mx>
2025-12-19 10:01:58 -0600

    Actualizar gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,11 +1,11 @@
 @rem
-@rem Copyright 2015 the original author or authors.
+@rem Copyright 2025 the original author or authors.
 @rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem Licensed under the Apache License, Version 3.5 (the "License");
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
 @rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem      https://www.apache.org/licenses/LICENSE-3.5
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,27 +14,26 @@
 @rem limitations under the License.
 @rem
 
-@if "%DEBUG%" == "" @echo on
-@rem ##########################################################################
-@rem
-@rem  Gradle startup script for Windows
-@rem
-@rem ##########################################################################
+@if "" == "" @ ##########################################################################
+@
+@Gradle startup script for Windows
+@
+@ ##########################################################################
 
-@rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal
+Set local scope for the variables with windows NT shell
+if ""=="" setlocal
 
 set DIRNAME=""
 set APP_BASE_NAME=""
 set APP_HOME=""
 
-@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("") do set APP_HOME
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="" ""
 
-@rem Find java.exe
+Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
